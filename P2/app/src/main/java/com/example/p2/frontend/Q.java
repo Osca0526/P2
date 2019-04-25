c8b821eda1293baaafa307b5c435dfa1b41c5f57
Christian <christian.bresson@hotmail.com>
2019-04-25 14:53:46 +0200

    .2

--- P2/app/src/main/java/com/example/p2/frontend/Q.java
+++ P2/app/src/main/java/com/example/p2/frontend/Q.java
@@ -1,10 +1,11 @@
-package com.example.p2;
+package com.example.p2.frontend;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
+import com.example.p2.R;
 
 public class Q extends AppCompatActivity{
 
